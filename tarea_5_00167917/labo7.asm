2b0c68ef19106f5c83856de16010b364246d6e0c
CristianAlfaro <00167917@uca.edu.sv>
2020-05-19 12:46:21 -0600

    Tarea finalizada

--- tarea_5_00167917/labo7.asm
+++ tarea_5_00167917/labo7.asm
@@ -1,13 +1,29 @@
-	org 	100h
+
+        org     100h
 
 section .text
 
+        call    setup
 	call 	texto	
 	call 	cursor
-	call 	phrase
+	call 	phrase1
+        call 	phrase2
+        call 	phrase3
 	call	kbwait
 
 	int 	20h
+
+setup:  mov     al, 7d
+        mov     [202h], al
+        mov     al, 00001111b
+        mov     [210h], al
+        mov     al, 00000111b
+        mov     [211h], al
+        mov     al, 00001110b
+        mov     [212h], al
+        mov     ax, 0000h
+        mov     [200h], ax
+        ret
 
 texto:	mov 	ah, 00h
 	mov	al, 03h
@@ -24,7 +40,7 @@ cursor: mov	ah, 01h
 w_char:	mov 	ah, 09h
 	mov 	al, cl
 	mov 	bh, 0h
-	mov 	bl, 00001111b
+	mov 	bl, 00000100b
 	mov 	cx, 1h
 	int 	10h
 	ret
@@ -35,21 +51,52 @@ kbwait: mov 	ax, 0000h
 
 m_cursr:mov 	ah, 02h
 	mov 	dx, di  ; columna
-	mov 	dh, 12d ; fila
+	mov 	dh, [202h] ; fila
 	mov 	bh, 0h
 	int 	10h
 	ret
 
-phrase:	mov 	di, 29d
-lupi:	mov 	cl, [msg+di-29d]
+jmp_row:mov     [200h], ax
+        mov     ax, 5h
+        add     [202h], ax
+        mov     ax, [200h]
+        ret
+
+phrase1:	mov 	di, 10d
+lupi1:	mov 	cl, [msg1+di-10d]
 	call    m_cursr
 	call 	w_char
 	inc	di
-	cmp 	di, len
-	jb	lupi
+	cmp 	di, len1
+	jb	lupi1
+        call    jmp_row
+	ret
+
+phrase2:	mov 	di, 45d
+lupi2:	mov 	cl, [msg2+di-45d]
+	call    m_cursr
+	call 	w_char
+	inc	di
+	cmp 	di, len2
+	jb	lupi2
+        call    jmp_row
+	ret
+
+phrase3:	mov 	di, 28d
+lupi3:	mov 	cl, [msg3+di-28d]
+	call    m_cursr
+	call 	w_char
+	inc	di
+	cmp 	di, len3
+	jb	lupi3
+        call    jmp_row
 	ret
 
 
 section .data
-msg	db 	"Mami que tu quiere? "
-len 	equ	$-msg+29d
+msg1	db 	"Hola, me podrias dar"
+len1 	equ	$-msg1+5d
+msg2	db 	"una nalguita feliz"
+len2 	equ	$-msg2+60d
+msg3	db 	"y a tu hermana?"
+len3 	equ	$-msg3+29d
